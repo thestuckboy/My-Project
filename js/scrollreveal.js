25d79252b48d990d0978056c44fed3a54882bec6
thestuckboy <josmanueljsg@gmail.com>
2021-05-22 18:04:52 -0500

    1.0

--- js/scrollreveal.js
+++ js/scrollreveal.js
@@ -2,3 +2,13 @@ ScrollReveal().reveal('.about-section div', {
     delay: 500,
     duration: 1000
 });
+
+ScrollReveal().reveal('.project-section div', {
+    delay: 500,
+    duration: 1000
+});
+
+ScrollReveal().reveal('.contact-section div', {
+    delay: 500,
+    duration: 1000
+});
